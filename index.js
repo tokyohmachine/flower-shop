3f9df4e10940493b2e6ef305312c5e26003e8e45
Daniela Moraes <97682748+tokyohmachine@users.noreply.github.com>
2023-07-24 11:58:42 -0300

    Commit new file index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+// The iamges appears in the shop-all page in grid
+// when i click on each image i will be directed to their page, where i can
+// see the information about that specific product
+
+window.onload = function() {
+    const source = document.getElementById('homeProducts').innerHTML;
+    const template = Handlebars.compile(source);
+
+const context = {
+    productHome: [
+      {
+      description: "bouquet",
+      image: "Images/shop-all/news-flowers/1.webp",
+      price: "$28.00",
+      
+      },
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/2.webp",
+        price: "$28.00",
+       
+      },
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/4.webp",
+        price: "$28.00",
+        
+      },
+      
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/5.webp",
+        price: "$75.00",
+        
+        
+      },
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/6.webp",
+        price: "$75.00",
+       
+        
+      },
+      
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/7.webp",
+        price: "$100.00",
+        
+      },
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/8.webp",
+        price: "$100.00",
+        
+      },
+      
+      {
+        description: "bouquet",
+        image: "Images/shop-all/news-flowers/9.webp",
+        price: "$100.00",
+        
+      }
+    
+  ]};
+
+  const compiledHome = template(context);
+  const containerHome = document.getElementById('homeShop');
+  containerHome.innerHTML = compiledHome;
+};
+
+
+
+$(document).ready(function() {
+  $("#menuToggle").click(function () {
+    $("#menu").toggle();
+   
+  });
+  
+});
